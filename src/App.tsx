1617b7322527500c1bffc051d669f1160db74fb8
Fra-Battaglia <francescobattaglia03@gmail.com>
2026-06-19 15:48:32 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+		<div className="container pb-8 px-8 md:px-0">
+			<h1 className="text-4xl font-black text-center my-8 text-(--text-h) uppercase tracking-wide">Mortgage Calculation</h1>
+		</div>
+    </>
+  )
+}
+
+export default App
